7ce115598364beeb57c681abb4a09bae2265941f
Anastasiya <nasta978@mail.ru>
2024-10-17 20:05:10 +0300

    feat: make a carousel, animation. (Thu, 17 Oct 2024 8:05:31 PM)

--- carousel/index.js
+++ carousel/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let i = 1;
+  const slides = document.querySelectorAll(".slide");
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClass();
+      slide.classList.add("active");
+    });
+  }
+  function clearActiveClass() {
+    slides.forEach((slide) => slide.classList.remove("active"));
+  }
 });
