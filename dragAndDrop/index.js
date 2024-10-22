e12491b45c0610edf1ea71b16513a15968a7cde4
Anastasiya <nasta978@mail.ru>
2024-10-22 18:55:53 +0300

    feat: capture the element and return to its original position. (Tue, 22 Oct 2024 6:55:21 PM)

--- dragAndDrop/index.js
+++ dragAndDrop/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let a;
+  const item = document.querySelector(".item");
+  const placeholders = document.querySelectorAll(".placeholder");
+
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
+
+  function dragStart(event) {
+    event.target.classList.add("hold");
+    setTimeout(() => event.target.classList.add("hide"), 0);
+  }
+  function dragEnd(event) {
+    event.target.classList.remove("hold");
+    event.target.classList.remove("hide");
+  }
 });
