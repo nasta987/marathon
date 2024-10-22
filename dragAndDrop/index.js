17ba445d18168851505bd04335ae621a71a57568
Anastasiya <nasta978@mail.ru>
2024-10-22 20:04:14 +0300

    feat: do dragging of elements and occupying a certain place (Tue, 22 Oct 2024 7:04:21 PM)

--- dragAndDrop/index.js
+++ dragAndDrop/index.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   item.addEventListener("dragstart", dragStart);
   item.addEventListener("dragend", dragEnd);
+  item.addEventListener("touchstart", touchStart);
+  item.addEventListener("touchmove", touchMove);
+  item.addEventListener("touchend", touchEnd);
+
+  for (const placeholder of placeholders) {
+    placeholder.addEventListener("dragover", dragover); // элемент находится над плэйсхолдером, куда мы можем поместить
+    placeholder.addEventListener("dragenter", dragenter); // элемент заходит на территорию конкретного плэйсхолдера
+    placeholder.addEventListener("dragleave", dragleave); //элемент вышел с территории плэйсхолдера
+    placeholder.addEventListener("drop", drop); // когда мы отпустили элемент
+  }
 
   function dragStart(event) {
     event.target.classList.add("hold");
@@ -12,5 +22,44 @@ document.addEventListener("DOMContentLoaded", function () {
   function dragEnd(event) {
     event.target.classList.remove("hold");
     event.target.classList.remove("hide");
+  }
+  function dragover(event) {
+    event.preventDefault(); // по умолчанию dragover запрещает перетаскивание и мы с помощью preventDefault отменяем запрет на перемещение
+  }
+  function dragenter(event) {
+    event.target.classList.add("hovered");
+  }
+  function dragleave(event) {
+    event.target.classList.remove("hovered");
+  }
+  function drop(event) {
+    // элемент не будет перетаскиваться без функции dragover
+    event.target.append(item); // добавить элемент на плэйсхолдер (event.target = class = plaiceholder)
+    event.target.classList.remove("hovered");
+  }
+  function touchStart(event) {
+    event.preventDefault();
+    dragStart(event.touches[0]);
+  }
+
+  function touchMove(event) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const target = document.elementFromPoint(touch.pageX, touch.pageY);
+    if (target && target.classList.contains("placeholder")) {
+      item.style.position = "absolute";
+      item.style.left = `${touch.pageX}px`;
+      item.style.top = `${touch.pageY}px`;
+    }
+  }
+
+  function touchEnd(event) {
+    event.preventDefault();
+    dragEnd(event.changedTouches[0]);
+    const touch = event.changedTouches[0];
+    const target = document.elementFromPoint(touch.pageX, touch.pageY);
+    if (target && target.classList.contains("placeholder")) {
+      target.append(item);
+    }
   }
 });
